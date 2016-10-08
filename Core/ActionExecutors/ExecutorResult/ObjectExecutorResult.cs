47b2a20c1116322f0e05799cbb2c8992a2991699
Peter Mazin <MaiznPetr@gmail.com>
2016-10-09 00:03:36 +0500

    функция возвращающая объект

---
+++ Core/ActionExecutors/ExecutorResult/ObjectExecutorResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Core;
+
+namespace Core.ActionExecutors.ExecutorResult
+{
+    public class ObjectExecutorResult : BaseExecutorResult
+    {
+        /// <summary>
+        /// Результат выполнения
+        /// </summary>
+        public ObjectInfo ExecutorResult { get; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="object">Объект</param>
+        /// <param name="executorState">Статус результата исполнения</param>
+        public ObjectExecutorResult(ObjectInfo @object ,EResultState executorState = EResultState.Success) : base(executorState)
+        {
+            ExecutorResult = @object;
+        }
+    }
+}
--- Core/ActionExecutors/GetObjectExecutor.cs
+++ Core/ActionExecutors/GetObjectExecutor.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using Core.ActionExecutors.ExecutorResult;
 using Core.ConfigEntity.ActionObjects;
 using Core.Core;
+using Core.Handlers;
 using Core.Helpers;
 
 namespace Core.ActionExecutors
@@ -12,6 +14,7 @@ namespace Core.ActionExecutors
     /// </summary>
     public class GetObjectExecutor : BaseExecutor
     {
+        readonly IWindowsProc _windowsProc = new NativeWindowsProc();
         /// <summary>
         /// Исполняет метод получения информации об указанном объекте
         /// </summary>
@@ -31,10 +34,50 @@ namespace Core.ActionExecutors
                 Status = EStatus.Info
             }, false);
 
-            if (actions.Count>1)
+            if (actions.Count > 1)
                 throw new Exception("Получить можно только один объект.");
 
-            throw new NotImplementedException();
+            if (actions.Count == 0 && previousResult != null)
+            {
+                //выбор особых сценариев дляразных результатов
+                //например перемещение относительно окна или еще чего то
+                switch (previousResult.GetType().Name)
+                {
+                    case nameof(ExpectWindowExecutorResult):
+                        {
+                            var expWin = (ExpectWindowExecutorResult)previousResult;
+
+                            if (expWin.State != EResultState.Success && expWin.ExecutorResult == null)
+                                throw new Exception("Ошибка относительного позиционирования, ExpectWindowExecutorResult не валиден");
+
+                            return new ObjectExecutorResult(_windowsProc.GetObjectFromPoint(expWin.ExecutorResult.Pos));
+                        }
+                    case nameof(CurrentMousePosExecutorResult):
+                        {
+                            var mousePos = (CurrentMousePosExecutorResult)previousResult;
+                            if (mousePos.State != EResultState.Success)
+                                throw new Exception("Ошибка относительного позиционирования, CurrentMousePosExecutorResult не валиден");
+                            var currentPos = mousePos.ExecutorResult;
+                            return new ObjectExecutorResult(_windowsProc.GetObjectFromPoint(currentPos));
+                        }
+                    case nameof(ObjectExecutorResult):
+                        {
+                            var mousePos = (ObjectExecutorResult)previousResult;
+                            if (mousePos.State != EResultState.Success)
+                                throw new Exception("Ошибка относительного позиционирования, ObjectExecutorResult не валиден");
+                            var currentPos = mousePos.ExecutorResult.Pos;
+                            return new ObjectExecutorResult(_windowsProc.GetObjectFromPoint(currentPos));
+                        }
+                    default:
+                        throw new NotSupportedException(previousResult.GetType().Name);
+                }
+            }
+
+            var objectAct = actions.Cast<GetObjectAct>().First();
+            if (!objectAct.ObjectPos.IsEmpty)
+                return new ObjectExecutorResult(_windowsProc.GetObjectFromPoint(objectAct.ObjectPos));
+
+            return new BaseExecutorResult(EResultState.NoResult);
         }
 
         public override IExecutorResult Invoke(IExecutorResult previousResult = null)
