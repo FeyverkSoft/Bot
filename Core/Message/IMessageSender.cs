1c1af0674750e3c7699790e245f01265841e103c
Peter Mazin <MaiznPetr@gmail.com>
2016-10-13 19:21:00 +0500

    IMessageSender

---
+++ Core/Message/IMessageSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Message
+{
+    /// <summary>
+    /// Интерфейс отправителя сообщений
+    /// </summary>
+    internal interface IMessageSender
+    {
+    }
+}
