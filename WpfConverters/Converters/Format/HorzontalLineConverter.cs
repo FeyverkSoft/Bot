19dc7e9d8b11ce04e9017a99e4a3bb4ee5b825f2
Mazin.Peter <maiznpetr@gmail.com>
2016-10-21 18:01:53 +0500

    /

---
+++ WpfConverters/Converters/Format/HorzontalLineConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace WpfConverters.Converters.Format
+{
+   public class HorzontalLineConverter: BaseValueConverterExtension
+    {
+        public override Object Convert(Object value, Type targetType, Object parameter, System.Globalization.CultureInfo culture)
+        {
+            TreeViewItem item = (TreeViewItem)value;
+            ItemsControl ic = ItemsControl.ItemsControlFromItemContainer(item);
+            var index = ic.ItemContainerGenerator.IndexFromContainer(item);
+
+            if ((string)parameter == "left")
+            {
+                return index == 0 ? 0 : 1;
+            }
+
+            return index == ic.Items.Count - 1 ? 0 : 1;
+        }
+    }
+}
---
+++ WpfConverters/Converters/Format/VerticalLineConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace WpfConverters.Converters.Format
+{
+    public class VerticalLineConverter : BaseValueConverterExtension
+    {
+        public override Object Convert(Object value, Type targetType, Object parameter, System.Globalization.CultureInfo culture)
+        {
+            TreeViewItem item = (TreeViewItem)value;
+            ItemsControl ic = ItemsControl.ItemsControlFromItemContainer(item);
+            //var index = ic.ItemContainerGenerator.IndexFromContainer(item);
+
+            if ((String)parameter != "top")
+                return item.HasItems == false ? 0 : 1;
+
+            return ic is TreeView ? 0 : 1;
+        }
+    }
+}
