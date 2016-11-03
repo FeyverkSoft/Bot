c40cca5839bb751623c7c37e40d4c393b1feb45b
Peter Mazin <MaiznPetr@gmail.com>
2016-11-04 01:47:58 +0500

    фикс KeyCodeControl

--- WpfExecutor/Control/KeyCodeControl.xaml.cs
+++ WpfExecutor/Control/KeyCodeControl.xaml.cs
@@ -2,7 +2,9 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using Core.Core;
 using WpfConverters.Converters.Common;
+using WpfExecutor.Helpers;
 using WpfExecutor.Model;
 
 namespace WpfExecutor.Control
@@ -20,12 +22,14 @@ namespace WpfExecutor.Control
         [UsedImplicitly]
         public static FrameworkElementFactory GetFrameworkElementFactory()
         {
+            var list = typeof(KeyCode).GeEnumTuple();
             var factory = new FrameworkElementFactory(typeof(KeyCodeControl));
+            factory.SetValue(ItemsSourceProperty, list);
             factory.SetBinding(SelectedItemProperty, new Binding(nameof(PropModel.Value))
             {
                 Mode = BindingMode.TwoWay,
                 UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
-                Converter = new TupleConverter(),
+                Converter = new TupleConverter(list),
                 ConverterParameter = nameof(Tuple<String, Object>.Item2)
             });
             return factory;
