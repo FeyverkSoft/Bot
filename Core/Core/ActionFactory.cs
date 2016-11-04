0e638b90937ffe5c91aa22978942aabbdf2f8b90
Peter Mazin <MaiznPetr@gmail.com>
2016-11-04 22:08:02 +0500

    чистка кода и динамика :D

--- Core/Core/ActionFactory.cs
+++ Core/Core/ActionFactory.cs
@@ -13,61 +13,24 @@ namespace Core.Core
         {
             var list = Assemblys.TypeList;
             list.AddRange(Assemblys.PluginsTypeList);
-            List<IAction> actions = new List<IAction>();
-            foreach (var type in list.Where(x => x.GetInterfaces().Contains(typeof(IAction)) && x.Name != nameof(BaseActionObject)))
+            var actions = new List<IAction>();
+            foreach (var type in list.Where(x => x.GetInterfaces()
+            .Contains(typeof(IAction)) && x.Name != nameof(BaseActionObject)))
             {
                 var memberInfos = type.GetProperty(nameof(BaseActionObject.ActionType));
                 if (memberInfos == null)
                     continue; ;
-                if ((ActionType)memberInfos.GetValue(null, null) == actionType)
-                {
-                    var obj = type.GetConstructor(Type.EmptyTypes)?.Invoke(null);
-                    if (obj != null)
-                        actions.Add((IAction)obj);
-                }
+                if ((ActionType)memberInfos.GetValue(null, null) != actionType) continue;
+                var obj = type.GetConstructor(Type.EmptyTypes)?.Invoke(null);
+                if (obj != null)
+                    actions.Add((IAction)obj);
             }
             return actions;
         }
 
         public static ActionType GetType(IAction action)
         {
-            switch (action.GetType().Name)
-            {
-                case nameof(MouseMoveAct):
-                    return ActionType.MouseMove;
-                case nameof(MouseSetPosAct):
-                    return ActionType.MouseSetPos;
-                case nameof(KeyBoardAct):
-                    return ActionType.KeyBoard;
-                case nameof(KeyBoardKeysAct):
-                    return ActionType.KeyBoardKeys;
-                case nameof(SleepAct):
-                    return ActionType.Sleep;
-                case nameof(LoopAct):
-                    return ActionType.Loop;
-                case nameof(PluginInvokeAct):
-                    return ActionType.PluginInvoke;
-                case nameof(IfAction):
-                    return ActionType.If;
-                case nameof(ExpectWindowAct):
-                    return ActionType.ExpectWindow;
-                case nameof(GetObjectAct):
-                    return ActionType.GetObject;
-                case nameof(ScreenShotAct):
-                    return ActionType.GetScreenshot;
-                case nameof(MockAction):
-                    return ActionType.Mock;
-                case nameof(SendMessageAct):
-                    return ActionType.SendMessage;
-                case nameof(LabelAct):
-                    return ActionType.Label;
-                case nameof(GoToAct):
-                    return ActionType.GOTO;
-                case nameof(StackAct):
-                    return ActionType.Stack;
-                default:
-                    return ActionType.PluginAct;
-            }
+            return (ActionType)action.GetType().GetProperty(nameof(BaseActionObject.ActionType)).GetValue(null, null);
         }
     }
 }
