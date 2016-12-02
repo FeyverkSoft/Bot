d5f3c2e962a293e0515b092a1f6d96b8be090550
Peter Mazin <MaiznPetr@gmail.com>
2016-12-03 02:34:41 +0500

    PluginNameControl

---
+++ WpfExecutor/Control/PluginNameControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Core;
+using WpfExecutor.Model;
+
+namespace WpfExecutor.Control
+{
+    /// <summary>
+    /// Логика взаимодействия для PluginNameControl.xaml
+    /// </summary>
+    public partial class PluginNameControl : ComboBox, ICustomControl
+    {
+        public PluginNameControl()
+        {
+            InitializeComponent();
+        }
+
+        [UsedImplicitly]
+        public static FrameworkElementFactory GetFrameworkElementFactory()
+        {
+            var list = Assemblys.PluginsList.Distinct().Select(x => x.Name);
+            var factory = new FrameworkElementFactory(typeof(PluginNameControl));
+            factory.SetValue(ItemsSourceProperty, list);
+            factory.SetBinding(SelectedItemProperty, new Binding(nameof(IPropModel.Value))
+            {
+                Mode = BindingMode.TwoWay,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+            });
+            return factory;
+        }
+    }
+}
